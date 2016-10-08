42fe1779dd0eb218628e824471cd92ea65196feb
Svetlana <svetlanak@mstlab.org>
2016-10-08 15:35:24 +0300

    Added gitignore

--- quiz/src/quiz/scripts/view/questionview.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="..\scripts\jquery.d.ts"/>
-/// <reference path="..\scripts\jqueryui.d.ts"/>
-(function ($) {
-    $.widget("quiz.questionview", {
-        options: {},
-    });
-}(jQuery));
-//# sourceMappingURL=questionview.js.map
--- quiz/src/quiz/scripts/view/resultview.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="..\scripts\jquery.d.ts"/>
-/// <reference path="..\scripts\jqueryui.d.ts"/>
-(function ($) {
-    $.widget("quiz.resultview", {
-        options: {},
-    });
-}(jQuery));
-//# sourceMappingURL=resultview.js.map
